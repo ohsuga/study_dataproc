6606cb56d2fd61ce338325834fdcd86ad237d949
ohsuga <ohsuga@gmail.com>
2016-11-30 11:40:34 +0900

    add package name and change dataproc settings

--- exec.sh
+++ exec.sh
@@ -8,7 +8,7 @@ sbt package
 
 # クラスタを作成
 gcloud beta dataproc clusters create ${CLUSTER_NAME} --bucket ${GCS_BUCKET} \
-       --zone asia-northeast1-a \
+       --zone asia-east1-a \
        --master-machine-type n1-standard-4 \
        --worker-machine-type n1-standard-4 \
        --num-workers 5
@@ -16,8 +16,8 @@ gcloud beta dataproc clusters create ${CLUSTER_NAME} --bucket ${GCS_BUCKET} \
 # Spark ジョブを実行
 gcloud beta dataproc jobs submit spark \
        --cluster ${CLUSTER_NAME} \
-       --class CalcRecommendItems \
-       --properties spark.dynamicAllocation.enabled=false,spark.executor.cores=5,spark.executor.memory=8g,spark.executor.instances=15 \
+       --class net.refabrik.studyDataproc.CalcRecommendItems \
+       --properties spark.dynamicAllocation.enabled=false,spark.executor.cores=3,spark.executor.memory=2g,spark.executor.instances=9 \
        --jars ./target/scala-2.11/calc-recommend-items_2.11-0.1.jar
 
 # クラスタを削除
